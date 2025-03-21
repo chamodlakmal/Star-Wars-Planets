f7cf3863d1cc18688d4cc02cbf20595f32141bd8
chamodlakmal <chamod.lakmal@pickme.lk>
2025-03-21 20:56:43 +0530

    use cases updates with the map function

--- app/src/main/java/lk/chamiviews/starwarsplanets/domain/usecase/GetNextPageUseCase.kt
+++ app/src/main/java/lk/chamiviews/starwarsplanets/domain/usecase/GetNextPageUseCase.kt
@@ -1,20 +1,14 @@
 package lk.chamiviews.starwarsplanets.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import lk.chamiviews.starwarsplanets.domain.model.PlanetPageData
 import lk.chamiviews.starwarsplanets.domain.model.toPlanetPageData
 import lk.chamiviews.starwarsplanets.domain.repository.PlanetRepository
 
 class GetNextPageUseCase(private val repository: PlanetRepository) {
-    operator fun invoke(nextPageUrl: String): Flow<Result<PlanetPageData>> = flow {
-        repository.getNextPage(nextPageUrl)
-            .collect { result ->
-                result.onSuccess {
-                    emit(Result.success(it.toPlanetPageData()))
-                }.onFailure {
-                    emit(Result.failure(it))
-                }
-            }
-    }
+    operator fun invoke(nextPageUrl: String): Flow<Result<PlanetPageData>> =
+        repository.getNextPage(nextPageUrl).map { result ->
+            result.mapCatching { it.toPlanetPageData() }
+        }
 }
--- app/src/main/java/lk/chamiviews/starwarsplanets/domain/usecase/GetPlanetsUseCase.kt
+++ app/src/main/java/lk/chamiviews/starwarsplanets/domain/usecase/GetPlanetsUseCase.kt
@@ -1,20 +1,14 @@
 package lk.chamiviews.starwarsplanets.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import lk.chamiviews.starwarsplanets.domain.model.PlanetPageData
 import lk.chamiviews.starwarsplanets.domain.model.toPlanetPageData
 import lk.chamiviews.starwarsplanets.domain.repository.PlanetRepository
 
 class GetPlanetsUseCase(private val repository: PlanetRepository) {
-    operator fun invoke(): Flow<Result<PlanetPageData>> = flow {
-        repository.getPlanets().collect { result ->
-            result.onSuccess {
-                emit(Result.success(it.toPlanetPageData()))
-            }.onFailure {
-                emit(Result.failure(it))
-            }
+    operator fun invoke(): Flow<Result<PlanetPageData>> =
+        repository.getPlanets().map { result ->
+            result.mapCatching { it.toPlanetPageData() }
         }
-    }
-
 }
